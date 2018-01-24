edf367ec62c01e8b9f346b2929498ee508ba0456
Eric Loos <ericloos00@gmail.com>
2018-01-23 21:55:22 -0600

    added json to include other models data

--- royalflush-api/app/controllers/api/address_unit_controller.rb
+++ royalflush-api/app/controllers/api/address_unit_controller.rb
@@ -1,6 +1,6 @@
 class Api::AddressUnitController < ApplicationController
   def index
-    @address_unit = AddressUnit.all
+    @address_unit = AddressUnit.all.as_json(include: [:address, :unit])
     render json: @address_unit
   end
 
